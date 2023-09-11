aa40653574922a829c9a2476271d1f008f2479fd
damitha sahan <sahanchandrasena462@gmail.com>
2023-09-11 23:12:39 +0530

    updates

---
+++ htdocs/largeText.php
@@ -0,0 +1,14 @@
+<?php
+// Large Text Example
+
+$largeText = <<<EOT
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tincidunt ante, id consectetur mi. Duis pharetra pharetra tincidunt. Aenean tincidunt risus ligula, ut blandit ante iaculis eget. Vestibulum a ante vel ex bibendum dignissim. Aliquam feugiat neque non ligula posuere efficitur. Vivamus sit amet efficitur eros. Vivamus tristique eget arcu vel convallis. Integer fermentum bibendum orci, at dignissim est hendrerit nec. Sed euismod lacus risus, sit amet scelerisque est varius eget. Aenean non viverra metus. Cras at sem sed orci congue tristique ut non sapien. Sed tristique ante velit, vel facilisis ex facilisis at.
+
+Proin ac velit eget dolor blandit blandit ut non metus. Vivamus eu lectus a lectus interdum tristique. Curabitur nec rhoncus lectus, in venenatis nulla. Nullam lacinia ex id orci vulputate, at laoreet urna interdum. Fusce vestibulum tristique risus non scelerisque. Praesent bibendum, ligula nec sollicitudin vestibulum, elit quam venenatis velit, eu eleifend arcu ex eu odio. Maecenas consectetur quam vel augue convallis mattis. Proin euismod, dolor eu mattis hendrerit, eros est suscipit turpis, non pharetra justo purus vel ligula.
+
+Suspendisse potenti. Sed ultricies elit sed est efficitur, vel malesuada lectus rhoncus. Donec ac bibendum purus. Sed viverra venenatis metus, id dignissim dolor dictum a. Vestibulum auctor lectus ut libero rhoncus, non rhoncus odio tincidunt. Nunc a luctus neque, eu vestibulum purus. Vivamus eu quam eu risus cursus malesuada non a odio. Etiam id suscipit odio, in vehicula eros. Sed non bibendum elit, non euismod odio. Donec vitae erat leo. Sed eu euismod augue, at cursus dolor. Pellentesque in metus fringilla, malesuada purus a, facilisis arcu. Quisque eleifend ante ut odio auctor, nec fermentum libero luctus. Duis nec massa sapien. Maecenas vel erat vitae nulla congue venenatis.
+
+EOT;
+
+echo $largeText;
+?>
